a399d04276c4c66d7fa096c4d7890103f09f23db
lisa56530 <145032722+lisa56530@users.noreply.github.com>
2023-10-18 18:05:26 +0200

    Update TicketMachine.java

--- src/main/java/ticketmachine/TicketMachine.java
+++ src/main/java/ticketmachine/TicketMachine.java
@@ -82,13 +82,22 @@ public class TicketMachine {
 	 * @return vrai si le ticket a été imprimé, faux sinon
 	 */
 	public boolean printTicket() {
-		// Simulate the printing of a ticket.
+		
+
+		if (this.balance == this.price) {
+			// Simulate the printing of a ticket.
 		System.out.println("##################");
 		System.out.println("# The BlueJ Line");
 		System.out.println("# Ticket");
 		System.out.println("# " + price + " cents.");
 		System.out.println("##################");
 		System.out.println();
-		return true;
+		balance = balance- price ; 
+		total = total + price ; 
+		return true ;
+
+		}
+
+		else return false;
 	}
 }
